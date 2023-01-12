4f433341531205d76143c243e4a91fb3e0014177
Amin <amin-hassairi@hotmail.fr>
2023-01-12 11:26:55 +0100

    toppings was created

---
+++ src/utils/toppings.js
@@ -0,0 +1,18 @@
+export const toppings = [
+    {
+        name: "Cheese",
+        price: 1.0
+    },
+    {
+        name: "Pepperoni",
+        price: 1.0
+    },
+    {
+        name: "Salami",
+        price: 1.0
+    },
+    {
+        name: "Bacon",
+        price: 1.0
+    }
+  ];
